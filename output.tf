f4fcf9ecf053ae27294620762fb896b1b162ceb9
eswarkum <108848157+eswarkum@users.noreply.github.com>
2022-07-15 12:21:25 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,20 @@
 output "cosmosdb_name" {
   description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.name
+}
+output "cosmosdb_id" {
+  description = "Cosmos db name"
   value       = azurerm_cosmosdb_account.db.id
 }
-output "azurerm_private_endpoint" {
-  description = "The private endpoint name"
-  value       = azurerm_private_endpoint.pe.id
+output "cosmosdb_endpoint" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.endpoint
+}
+output "cosmosdb_connectionstrings" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.cosmosdb_connectionstrings
+}
+output "cosmosdb_masterkey" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.primary_master_key
 }
