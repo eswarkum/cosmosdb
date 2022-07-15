cc98468e54e69d51e8b2cda40779b08e4905bd07
eswarkum <108848157+eswarkum@users.noreply.github.com>
2022-07-15 18:05:45 +0530

    Add files via upload

---
+++ output.tf
@@ -0,0 +1,21 @@
+output "cosmosdb_name" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.name
+}
+output "cosmosdb_id" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.id
+}
+output "cosmosdb_endpoint" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.endpoint
+}
+output "cosmosdb_connectionstrings" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.connectionstrings
+}
+
+output "cosmosdb_masterkey" {
+  description = "Cosmos db name"
+  value       = azurerm_cosmosdb_account.db.primary_key
+}
